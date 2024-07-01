64d574c6b456d409a962eb988b57cbb1d7d66ef2
Filip Kloska <fkloska1@gmail.com>
2024-07-01 19:41:34 +0200

    01.07.2024

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss', // Our new line you can change app.scss to whatever.scss
                 'resources/js/app.js',
             ],
             refresh: true,
